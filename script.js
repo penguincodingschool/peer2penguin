d55905ae1185626ead906fbde9e5aced9db232b7
penguinluis <93602442+penguinluis@users.noreply.github.com>
2021-12-14 11:21:13 -0500

    update script.js

--- script.js
+++ script.js
@@ -1,4 +1,3 @@
-
 // *********BEGIN PeerJS STUFF
 
 let Peer = window.Peer;
@@ -35,6 +34,7 @@ function onConnectButtonClicked(){
     localStatusEl.innerHTML = conn.peer
     // receive data
     iAmHost = false;
+    successConnectToHostAnimation()
     console.log(`opened connection ${conn}`);
     conn.on('data', function(data){
       incomingDict = data;
@@ -62,6 +62,7 @@ peer.on('connection', function(incomingConn) {
     sendDict()
     dataInterval = setInterval(sendDict, 10);
     iAmHost = true;
+    successfulConnectAsHostAnimation()
   })
   
   conn.on('data', function(data){
@@ -70,104 +71,154 @@ peer.on('connection', function(incomingConn) {
       incomingDict = data;
   });
 });  
-
-
-
 // END PeerJs STUFF
+
 
 // BEGIN TEMPLATING STUFF
 
-const dropInWidth = "33vw"
-const dropInRight = `calc(-${dropInWidth} + 1em)`
-
-const templateRawHTML = `
+const roomJoinTemplateRawHTML = `
     <template id="parent-template" style="display: none;">
-    <style>
+    
+    <div id="drop-in-container"><style>
       #room-id-input{
-        margin-left: 10px;
+        margin-left: -3px;
         width: 50px;
-        height: 1em;
+        border-radius: 5px;
+        border: 1px solid black;
+        font-family: Times New Roman;
       }
-      #connect-to-room-button{
-        margin-left: 10px;
+      #room-id-input:focus{
+        outline: 1px solid black;
+      }
+      #connect-to-room{
+        background-color: white;
+        padding: 0.25em 0.5em 0.25em, 0.5em;
+        border: 1px solid black;
+        border-radius: 5px;
+        font-family: Times New Roman
+      }
+      #connect-to-room:hover{
+        background-color: #F0F0F0;
+        box-shadow: 1px 1px 0px black;
+        cursor: pointer;
       }
       #drop-in-container{
-        font-size: 1.5em;
+        font-size: 100%;
         display: flex;
+        flex-wrap: wrap;
         position: fixed;
-        height: 1em;
-        width: ${dropInWidth};
-        right: ${dropInRight};
-        top: 5px;
+        top: 10px;
         background: #ffae09;
-        border: 1px solid orange;
-        border-radius: 0.25em;
-        padding: 0.25em 0.5em 0.25em 0.25em;
+        border: 1px solid black;
+        border-radius: 0.1em;
+        padding: 0.25em 0.25em 0.25em 0.25em;
         align-items: center;
-        transition: all 0.5s ease;
-        box-shadow: 0px 0px 5px gray;
+        box-shadow: 0px 0px 5px lightgray;
+        gap: 5px;
       }
       #drop-in-toggle{
-        height: 1em;
-        width: 1em;
+        height: 1.75em;
+        width: 1.75em;
         cursor: pointer;
         border-radius: 100%;
         transition: all 0.5s ease;
+        transform: rotate(90deg);
       }
       #drop-in-toggle:hover{
         box-shadow: 0px 0px 10px white;
       }
       .drop-in-flex-child{
+        font-size: 2.5vh;
         flex: 0 1 auto;
         flex-wrap: wrap;
         margin: 0px 0.25em 0px 0.25em;
       }
+      .smooth{
+        transition: all 0.25s ease;
+      }
+      .hides{
+        display: none;
+      }
     </style>
-    <div id="drop-in-container">
-      <img src="left.svg" class="drop-in-flex-child" id="drop-in-toggle"/>
-      <span class="drop-in-flex-child">
+      <img src="https://penguincodingschool.github.io/peer2penguin/left.svg" class="drop-in-flex-child smooth" id="drop-in-toggle"/>
+      <span class="drop-in-flex-child hides smooth">
         join a room:
       </span>
-      <input id="room-id-input" class="drop-in-flex-child"></input>
+      <input 
+      id="room-id-input" 
+      class="drop-in-flex-child hides smooth"
+      autocomplete="off"
+      ></input>
       <button 
       id="connect-to-room"
-      class="drop-in-flex-child"
-      onclick="onConnectButtonClicked()">join</button>
-      <div class="drop-in-flex-child">
+      class="drop-in-flex-child hides smooth"
+      onclick="onConnectButtonClicked()">go</button>
+      <div class="drop-in-flex-child hides smooth">
       you are in room: <span id="localStatus">loading...</span></div>
     </div>
     </template>`
 
 
-var connect_to_room_button;
-var room_id_input;
-var localStatusEl;
-var toggleJoinMenu;
-var dropInContainer;
-var expanded = false;
-var shared;
+
+let connect_to_room_button;
+let room_id_input;
+let localStatusEl;
+let toggleJoinMenu;
+let dropInContainer;
+let expanded = false;
+let thingsToHide;
 
 // create and add template for use by pen2guin
-var body = document.body
 function init(){
-  body.innerHTML += templateRawHTML
+  document.body.innerHTML += roomJoinTemplateRawHTML
   let templateEl = document.getElementById('parent-template')
-  let clone = templateEl.content.cloneNode(true)
+  let clone = templateEl.content.firstElementChild.cloneNode(true)
 
 
-  connect_to_room_button = clone.getElementById("connect-to-room")
-  room_id_input = clone.getElementById("room-id-input")
-  localStatusEl = clone.getElementById('localStatus')
-  toggleJoinMenu = clone.getElementById('drop-in-toggle')
-  dropInContainer = clone.getElementById('drop-in-container')
-  shared = document.getElementById("shared")
+  connect_to_room_button = clone.querySelector("#connect-to-room")
+  room_id_input = clone.querySelector("#room-id-input")
+  localStatusEl = clone.querySelector('#localStatus')
+  toggleJoinMenu = clone.querySelector('#drop-in-toggle')
+  dropInContainer = clone
 
+  document.body.appendChild(clone)
+
+  thingsToHide = Array.from(document.getElementsByClassName("hides"))
 
   toggleJoinMenu.addEventListener("click", function(){
-    dropInContainer.style.right = expanded ? dropInRight : "-10px";
-    dropInContainer.style.height = expanded ? "1em" : "2em";
-    toggleJoinMenu.style.transform = expanded ? "none" : "rotate(180deg)";
+    if (expanded){
+      //dropInContainer.style.right = dropInRight;
+      toggleJoinMenu.style.transform = "rotate(90deg)";
+      thingsToHide.forEach(el => {el.style.display = "none"})
+    } else {
+      //dropInContainer.style.right = "-10px";
+      toggleJoinMenu.style.transform = "rotate(180deg)";
+      
+      thingsToHide.forEach(el => {el.style.display = "inline-flex"})
+    }
     expanded = !expanded
   })
-  document.body.appendChild(clone)
+}
+
+/* for an onConnect animation - todo */
+
+const connectToastRawHTML = `
+  <template id="toast-template">
+    <div id="parent-toast">
+    <style>
+      #parent-toast{
+        width: 400px;
+      }
+    </style>
+    </div>  
+  </template>`
+
+
+
+function successfulConnectAsHostAnimation(){
+
+}
+
+function successConnectToHostAnimation(){
+  
 }
